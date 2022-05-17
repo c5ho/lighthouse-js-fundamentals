84e50f279fd64163d2b6c0da18076ae805cad24c
Calvin Ho <calvin.ch.ho@gmail.com>
2022-05-16 22:47:30 -0400

    Odd checker function

---
+++ odd.js
@@ -0,0 +1,14 @@
+const isOdd = function (num) {
+  return num % 2 !== 0;
+}
+
+/*
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
